09f186756da1ebfaf89381bb74f48bdc88da93bd
morshedul-antor <antor.morshedul@gmail.com>
2023-06-20 17:27:53 +0600

    feat: create tables

--- create_category_table.py
+++ create_category_table.py
@@ -4,10 +4,11 @@ mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="medicines"
+    database="categories"
 )
 
 mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE medicine_list (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, generic VARCHAR(255) NOT NULL, form VARCHAR(255) NOT NULL, strength VARCHAR(255) NOT NULL, pharmaceuticals VARCHAR(255) NOT NULL, unit_type VARCHAR(255) NOT NULL, unit_price FLOAT NOT NULL)")
+mycursor.execute(
+    "CREATE TABLE category (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL)")
 
 print('Table created...')
